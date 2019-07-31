1a5f220c896ab9fc94b4d929d697ba591b4c42a8
Zion Perez <zionperez@gmail.com>
2019-08-01 00:05:38 +0900

    Add scene changer script (need to test and implement). Add digiffiti font. Add main menu. Implement bouncy text. Add title: Space Breaker with bouncy text.

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneChanger : MonoBehaviour
+{
+    private Animator animator;
+    private int sceneToLoad;
+
+    // Singleton pattern
+    // https://gamedev.stackexchange.com/a/116010/123894
+    private static SceneChanger _instance;
+    public static SceneChanger Instance { get { return _instance; } }
+
+    private void Awake()
+    {
+        // Singleton Enforcement Code
+        if (_instance != null && _instance != this)
+        {
+            Destroy(this.gameObject);
+            return;
+        }
+        else
+        {
+            _instance = this;
+        }
+        DontDestroyOnLoad(gameObject);
+    }
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    public void FadetoScene(int sceneIndex)
+    {
+        sceneToLoad = sceneIndex;
+        animator.SetTrigger("FadeOut");
+    }
+
+    public void OnFadeComplete()
+    {
+        SceneManager.LoadScene(sceneToLoad);
+        animator.SetTrigger("FadeIn");
+    }
+}
---
+++ Assets/TextSequenceAnimator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextSequenceAnimator : MonoBehaviour
+{
+    public float charDelay;
+    public float totalAnimationTime;    
+
+    private List<Animator> textAnimators;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        textAnimators = new List<Animator>(GetComponentsInChildren<Animator>());
+        StartCoroutine(AnimateText());
+    }
+
+    private IEnumerator AnimateText()
+    {
+        while (true)
+        {
+            float startTime = Time.time;            
+            foreach (var animator in textAnimators)
+            {
+                animator.SetTrigger("Bounce");
+                yield return new WaitForSeconds(charDelay);
+            }
+            float bounceTime = Time.time - startTime;            
+            yield return new WaitForSeconds(totalAnimationTime - bounceTime);
+        }
+    }
+}

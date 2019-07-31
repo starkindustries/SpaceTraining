10de231a096c831602252f32a8b67c432cf8389e
Zion Perez <zionperez@gmail.com>
2019-08-01 05:41:12 +0900

    Add scrolling background. Implement bouncy text animation.

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach(Touch touch in Input.touches)
+        {
+            if (touch.phase == TouchPhase.Ended)
+            {
+                Debug.Log("Touch lifted. Start Game!");
+            }
+        }
+
+        if(Input.GetMouseButtonUp(0))
+        {
+            Debug.Log("Mouse click. Start Game!");
+        } 
+    }
+}
---
+++ Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour
+{
+    public float scrollSpeed;
+    public int offsetMultiplier;
+
+    private float backgroundWidth;
+    private float xBoundary;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Vector3 screenBounds = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width, Screen.height, Camera.main.transform.position.z));
+        xBoundary = screenBounds.x;
+
+        backgroundWidth = GetComponent<SpriteRenderer>().size.x;
+        
+        // Set scroll speed velocity
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-scrollSpeed, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float offscreenPosition = xBoundary + backgroundWidth / 2;
+        if (transform.position.x < offscreenPosition * -1)
+        {
+            RepositionBackground();
+        }        
+    }
+
+    private void RepositionBackground()
+    {
+        Vector2 groundOffset = new Vector2(backgroundWidth * offsetMultiplier, 0);
+        transform.position = (Vector2)transform.position + groundOffset;
+    }
+}

eab0a6fe2e8daee3b8b35e7750ba34379062c093
Zion Perez <zionperez@gmail.com>
2019-08-03 01:24:02 +0900

    Implement shift blocks down.

--- Assets/Scripts/BrickBreaker.cs
+++ Assets/Scripts/BrickBreaker.cs
@@ -11,9 +11,10 @@ public static class BrickBreaker
     // skipPercentage:  the chance that a block should be blank/skipped
     // origin:          the x, y position of the topLeft block to use as an anchor for the rest of the blocks
     // parent:          the tilemap to use as the parent obect
-    public static Tilemap GenerateRowOfBlocks(int rowLength, GameObject[] blocks, float skipPercentage, Vector3 origin, Tilemap parent)
+    public static List<GameObject> GenerateRowOfBlocks(int rowLength, GameObject[] blocks, float skipPercentage, Vector3 origin, Tilemap parent)
     {
         List<int> blocksIndexList = GenerateRowOfIndices(rowLength, blocks.Length, skipPercentage);
+        List<GameObject> blocksList = new List<GameObject>();
 
         for (int i = 0; i < blocksIndexList.Count; i++)
         {
@@ -25,12 +26,12 @@ public static class BrickBreaker
             else
             {
                 Vector3 position = origin + new Vector3(x: 1, y: 0) * i;
-                Debug.Log(position.ToString());
-                GameObject.Instantiate(original: blocks[index], position: position, rotation: Quaternion.identity, parent: parent.transform);
+                GameObject block = GameObject.Instantiate(original: blocks[index], position: position, rotation: Quaternion.identity, parent: parent.transform);
+                blocksList.Add(item: block);
             }
         }
 
-        return parent;
+        return blocksList;
     }
 
     // Returns an array of integer indices. If the index is -1 then the the block is skipped.
@@ -63,5 +64,20 @@ public static class BrickBreaker
             }
         }
         return indices;
+    }
+
+    public static void ShiftBlocksDown(List<GameObject> blocks)
+    {        
+        foreach(GameObject block in blocks)
+        {
+            // TODO: do a try catch 
+            /*
+             *  MissingReferenceException: The object of type 'GameObject' has been destroyed but you are still trying to access it.
+             *  Your script should either check if it is null or you should not destroy the object.
+             *  BrickBreaker.ShiftBlocksDown (System.Collections.Generic.List`1[T] blocks) (at Assets/Scripts/BrickBreaker.cs:73)
+             *  EndlessMode.Update () (at Assets/Scripts/EndlessMode.cs:36)
+             */
+            block.transform.position += new Vector3(x: 0, y: -1);
+        }
     }
 }
--- Assets/Scripts/EndlessMode.cs
+++ Assets/Scripts/EndlessMode.cs
@@ -8,27 +8,34 @@ public class EndlessMode : MonoBehaviour
     public GameObject[] blockPrefabs;
     public Tilemap tilemap;
     public int levelWidth;
+    public float blockShiftInterval;
 
     // Origin is the grid coordinate of the top left block position,
     // which all other blocks can be positioned off of via an offset
-    private Vector3 origin = new Vector3(x: -0.5f, y: 23.5f);    
+    private Vector3 origin = new Vector3(x: -0.5f, y: 23.5f);
+    private float timeSinceLastBlockShift;
+
+    private List<GameObject> blocks;
 
     // Start is called before the first frame update
     private void Start()
     {
-        // Create three rows of blocks from the top 
-        tilemap = BrickBreaker.GenerateRowOfBlocks(rowLength: levelWidth, blocks: blockPrefabs, skipPercentage: 0f, origin: origin, parent: tilemap);       
+        blocks = new List<GameObject>();
+        timeSinceLastBlockShift = 0;
     }
 
     // Update is called once per frame
     private void Update()
     {
         // on criteria, move old blocks down one level
-        // generate new round of blocks above
-    }
-
-    private void MoveBricksDown()
-    {
-
+        // and generate new round of blocks above
+        timeSinceLastBlockShift += Time.deltaTime;
+        if (timeSinceLastBlockShift > blockShiftInterval)
+        {
+            timeSinceLastBlockShift = 0;
+            BrickBreaker.ShiftBlocksDown(blocks: blocks);
+            List<GameObject> row = BrickBreaker.GenerateRowOfBlocks(rowLength: levelWidth, blocks: blockPrefabs, skipPercentage: 0f, origin: origin, parent: tilemap);
+            blocks.AddRange(row);
+        }        
     }
 }

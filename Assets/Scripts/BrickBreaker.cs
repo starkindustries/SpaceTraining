99fd54555da8fea8d8daff2ee15d9e778bd3e548
Zion Perez <zionperez@gmail.com>
2019-08-02 13:31:17 +0900

    Implement function: GenerateRowOfBlocks in BrickBreaker static class. Refactor EndlessMode to use BrickBreaker class.

---
+++ Assets/Scripts/BrickBreaker.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+
+public static class BrickBreaker
+{
+    // Generates a row of block gameObjects. 
+    // rowLength:       the number of blocks (including blank blocks) to generate
+    // blocks:          the different block prefabs to choose from
+    // skipPercentage:  the chance that a block should be blank/skipped
+    // origin:          the x, y position of the topLeft block to use as an anchor for the rest of the blocks
+    // parent:          the tilemap to use as the parent obect
+    public static Tilemap GenerateRowOfBlocks(int rowLength, GameObject[] blocks, float skipPercentage, Vector3 origin, Tilemap parent)
+    {
+        List<int> blocksIndexList = GenerateRowOfIndices(rowLength, blocks.Length, skipPercentage);
+
+        for (int i = 0; i < blocksIndexList.Count; i++)
+        {
+            int index = blocksIndexList[i];
+            if (index < 0)
+            {
+                // skip block if index is -1
+            }
+            else
+            {
+                Vector3 position = origin + new Vector3(x: 1, y: 0) * i;
+                Debug.Log(position.ToString());
+                GameObject.Instantiate(original: blocks[index], position: position, rotation: Quaternion.identity, parent: parent.transform);
+            }
+        }
+
+        return parent;
+    }
+
+    // Returns an array of integer indices. If the index is -1 then the the block is skipped.
+    // Skip percentage is the chance that a block should be blank/skipped.
+    // Skip percentage should be between 0 (0%, never skip) and 1 (100%, always skip)
+    private static List<int> GenerateRowOfIndices(int rowLength, int blockCount, float skipPercentage)
+    {
+        List<int> indices = new List<int>();
+
+        if (skipPercentage < 0 || skipPercentage > 1)
+        {
+            Debug.LogError("Incorrect skipPercentage value: " + skipPercentage + ". Value should be between 0 and 1 inclusive.");
+            skipPercentage = 0;
+        }
+
+        for (int i = 0; i < rowLength; i++)
+        {
+            // Check if this block should be blank/skipped
+            float shouldSkip = Random.Range(0f, 1f);
+            if (shouldSkip < skipPercentage)
+            {
+                // Skip block
+                indices.Add(item: -1);
+            }
+            else
+            {
+                // Add a random block
+                int index = Random.Range(0, blockCount);
+                indices.Add(item: index);
+            }
+        }
+        return indices;
+    }
+}
--- Assets/Scripts/EndlessMode.cs
+++ Assets/Scripts/EndlessMode.cs
@@ -9,21 +9,15 @@ public class EndlessMode : MonoBehaviour
     public Tilemap tilemap;
     public int levelWidth;
 
-    // This is the grid coordinate of the top left block position,
+    // Origin is the grid coordinate of the top left block position,
     // which all other blocks can be positioned off of via an offset
-    private Vector3 topLeftBlockPosition = new Vector3(x: -0.5f, y: 23.5f);
-
-    // These are for convenience when adding or subtracting position offsets.
-    private Vector3 x1 = new Vector3(x: 1, y: 0);
-    private Vector3 y1 = new Vector3(x: 0, y: 1);
+    private Vector3 origin = new Vector3(x: -0.5f, y: 23.5f);    
 
     // Start is called before the first frame update
     private void Start()
     {
         // Create three rows of blocks from the top 
-        GameObject block = Instantiate(original: blockPrefabs[0], position: new Vector3(.5f, 23.5f), rotation: Quaternion.identity, parent: tilemap.transform);
-        // blocks[0]
-        block.transform.position = block.transform.position + new Vector3(x: 1, y: 0);
+        tilemap = BrickBreaker.GenerateRowOfBlocks(rowLength: levelWidth, blocks: blockPrefabs, skipPercentage: 0f, origin: origin, parent: tilemap);       
     }
 
     // Update is called once per frame
@@ -33,31 +27,8 @@ public class EndlessMode : MonoBehaviour
         // generate new round of blocks above
     }
 
-    private void GenerateRowOfBlocks()
-    {
-
-    }
-
     private void MoveBricksDown()
     {
 
     }
-    /*
-    public void Paint(GridLayout grid, GameObject brushTarget, Vector3Int position)
-    {
-        // Do not allow editing palettes
-        if (brushTarget.layer == 31)
-            return;
-
-        int index = Mathf.Clamp(Mathf.FloorToInt(GetPerlinValue(position, m_PerlinScale, k_PerlinOffset) * m_Prefabs.Length), 0, m_Prefabs.Length - 1);
-        GameObject prefab = m_Prefabs[index];
-        GameObject instance = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
-        if (instance != null)
-        {
-            Undo.MoveGameObjectToScene(instance, brushTarget.scene, "Paint Prefabs");
-            Undo.RegisterCreatedObjectUndo((Object)instance, "Paint Prefabs");
-            instance.transform.SetParent(brushTarget.transform);
-            instance.transform.position = grid.LocalToWorld(grid.CellToLocalInterpolated(new Vector3Int(position.x, position.y, m_Z) + new Vector3(.5f, .5f, .5f)));
-        }
-    }*/
 }

55e4235245aab3cd3e42f17f63bc47ad6b7c03fe
Zion Perez <zionperez@gmail.com>
2019-08-13 16:58:22 +0900

    Add SaveSystem, PlayerData, and GameManager scripts. Remove EngineFixedJoint and FinishLine scripts (unused). Reorganize scripts into folders.

---
+++ Assets/Scripts/Persistence/SaveSystem.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;    
+
+public static class SaveSystem
+{
+    private static string dataFileName = "/player.db";
+
+    public static void SavePlayerData(Player player)
+    {
+        BinaryFormatter formatter = new BinaryFormatter();
+        string path = Application.persistentDataPath + dataFileName;
+        FileStream stream = new FileStream(path, FileMode.Create);
+
+        PlayerData playerData = new PlayerData(player);
+        formatter.Serialize(stream, playerData);
+        stream.Close();
+    }
+
+    public static PlayerData LoadPlayerData()
+    {
+        string path = Application.persistentDataPath + dataFileName;
+        if (File.Exists(path))
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            FileStream stream = new FileStream(path, FileMode.Open);
+            PlayerData data = formatter.Deserialize(stream) as PlayerData;
+            stream.Close();
+            return data;
+        }
+        else
+        {
+            Debug.LogError("Save file not found in " + path);
+            return null;
+        }
+    }
+}
